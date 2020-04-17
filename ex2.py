d71472e09cd19f006d758b7344b959f74b667824
J.Freeman <34489383+jfreeman812@users.noreply.github.com>
2020-04-17 14:47:51 -0500

    Add files via upload

---
+++ ex2.py
@@ -0,0 +1,8 @@
+# A comment, this is so you can read your program later.
+#A Anything after the '#' is ignored by python
+
+print ('I could have code like this') # and the comment after is ognored.
+
+# You can also use a comment to "disable" or comment out code:
+# print ('This won't run.')
+print('This will run.')
